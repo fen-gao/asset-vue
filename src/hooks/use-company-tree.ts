fb98c9d66420572349dbdf5dbc472be3bb6c4884
fen-gao <fernando.fnn97@gmail.com>
2024-08-26 21:52:12 -0300

    feat(hooks): add custom hooks for company data management
    - Create use-company-context.ts hook for accessing company context
    - Implement use-company-tree.ts hook for handling company tree structure
    - Organize custom hooks in dedicated 'hooks' folder for better modularity

---
+++ src/hooks/use-company-tree.ts
@@ -0,0 +1,54 @@
+import { useQuery } from '@tanstack/react-query'
+import { fetchAssetsByCompany, fetchLocationsByCompany } from '../service/api'
+import { useMemo } from 'react'
+
+import constructCompanyHierarchy, { CompanyEntity } from '../utils/compose-tree'
+import { Company, FilterType } from '../context/type'
+import filterCompanyTree from '../utils/filter-tree'
+
+interface UseCompanyTreeProps {
+  activeCompany: Company | null
+  activeFilter: FilterType
+  search: string
+}
+
+interface CompanyTree {
+  tree: CompanyEntity[]
+  map: Map<string, CompanyEntity>
+}
+
+export default function useCompanyTree({ activeCompany, activeFilter, search }: UseCompanyTreeProps) {
+  const { data: assets, isLoading: isLoadingAssets } = useQuery({
+    queryKey: ['assets', activeCompany?.id],
+    queryFn: () => fetchAssetsByCompany(activeCompany!.id),
+    enabled: !!activeCompany,
+  })
+
+  const { data: locations, isLoading: isLoadingLocations } = useQuery({
+    queryKey: ['locations', activeCompany?.id],
+    queryFn: () => fetchLocationsByCompany(activeCompany!.id),
+    enabled: !!activeCompany,
+  })
+
+  const companyTree = useMemo<CompanyTree>(() => {
+    if (assets && locations) {
+      const builtHierarchy = constructCompanyHierarchy(locations, assets)
+      return {
+        tree: builtHierarchy.roots,
+        map: builtHierarchy.entityLookup,
+      }
+    }
+    return { tree: [], map: new Map<string, CompanyEntity>() }
+  }, [assets, locations])
+
+  const listUnitsFiltered = useMemo(
+    () => filterCompanyTree(companyTree.tree, { search, activeFilter }),
+    [search, activeFilter, companyTree.tree]
+  )
+
+  return {
+    companyRoot: listUnitsFiltered,
+    companyNodes: companyTree.map,
+    isLoading: isLoadingAssets || isLoadingLocations,
+  }
+}
